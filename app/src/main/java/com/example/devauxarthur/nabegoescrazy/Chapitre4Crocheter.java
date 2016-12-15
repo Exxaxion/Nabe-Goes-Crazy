de0774c3177e39cad1cc7d5e708e8ff2ac570697
apprenti <pchbenoit31@gmail.com>
2016-12-15 18:02:33 +0100

    premier commit

---
+++ app/src/main/java/com/example/devauxarthur/nabegoescrazy/Chapitre4Crocheter.java
@@ -0,0 +1,13 @@
+package com.example.devauxarthur.nabegoescrazy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chapitre4Crocheter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapitre4_crocheter);
+    }
+}
---
+++ app/src/main/java/com/example/devauxarthur/nabegoescrazy/Chapitre7TheEnd.java
@@ -0,0 +1,13 @@
+package com.example.devauxarthur.nabegoescrazy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chapitre7TheEnd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapitre7_the_end);
+    }
+}
