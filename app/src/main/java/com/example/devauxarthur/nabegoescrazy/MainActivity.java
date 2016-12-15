00e57d585d535502010886cb4541258e7a5d6b5a
apprenti <pchbenoit31@gmail.com>
2016-12-15 20:11:45 +0100

    ajout synopsis

--- app/src/main/java/com/example/devauxarthur/nabegoescrazy/MainActivity.java
+++ app/src/main/java/com/example/devauxarthur/nabegoescrazy/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.devauxarthur.nabegoescrazy;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Timer;
@@ -17,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = (Button) findViewById(R.id.idbutton);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent= new Intent(MainActivity.this, Synopsis.class);
+                startActivity(intent);
+            }
+        });
         }
     }
--- app/src/main/java/com/example/devauxarthur/nabegoescrazy/Synopsis.java
+++ app/src/main/java/com/example/devauxarthur/nabegoescrazy/Synopsis.java
@@ -1,7 +1,11 @@
 package com.example.devauxarthur.nabegoescrazy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Synopsis extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class Synopsis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synopsis);
+
+        ImageView image = (ImageView) findViewById(R.id.idSynopsis);
+        Button button = (Button) findViewById(R.id.idbutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Synopsis.this, Chapitre1Bureau.class);
+                startActivity(intent);
+            }
+        });
     }
 }
